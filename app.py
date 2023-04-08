4841edba83e45972c08bac13da0fe58ae8c70efb
brandon <brandonsurh@gmail.com>
2023-04-07 20:30:29 -0400

    Upgraded OpenAI model

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 #print(openai.api_key)
 
 from flask import Flask, render_template, request
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 def index():
@@ -14,17 +14,22 @@ def index():
 @app.route("/get_response", methods=["POST"])
 def get_response():
     user_input = request.form["user_input"]
-
+    
     # Call ChatGPT API
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=user_input,
-        max_tokens=100,
-        n=1,
-        stop=None,
-        temperature=0.5,
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        #model="text-davinci-003",
+        messages = [
+                {"role": "user", "content": '''You are an AI personal assistant. 
+                                                Answer like a normal verbal conversation. Be very casual like you are another human.
+                                                Keep responses to the point and brief unless specified otherwise.'''},
+                {"role": "user", "content": 'You are my personal assistant. I will ask you anything I want to know.'},
+                {"role": "user", "content": user_input}
+            ]
     )
 
     # Extract and return the chatbot's response
-    chatbot_response = response.choices[0].text.strip()
-    return chatbot_response
+    result = ''
+    for choice in response.choices:
+        result += choice.message.content
+    return result
