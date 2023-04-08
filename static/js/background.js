e0898a845a91d60a08968c6fdfa8282e378ebfb7
brandon <brandonsurh@gmail.com>
2023-04-07 21:05:14 -0400

    Edited styling, background changes

--- static/js/background.js
+++ static/js/background.js
@@ -1,45 +1,27 @@
-(function () {
+window.onload = function() {
+    // Set up canvas
     const canvas = document.createElement('canvas');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    canvas.style.position = 'fixed';
+    canvas.style.top = '0';
+    canvas.style.left = '0';
+    canvas.style.pointerEvents = 'none';
     document.body.appendChild(canvas);
-
+  
     const ctx = canvas.getContext('2d');
-
-    class Circle {
-        constructor(x, y, radius) {
-            this.x = x;
-            this.y = y;
-            this.radius = radius;
-        }
-
-        draw() {
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-            ctx.fillStyle = 'rgba(255, 77, 77, 0.05)';
-            ctx.fill();
-            ctx.strokeStyle = 'rgba(255, 77, 77, 0.1)';
-            ctx.stroke();
-        }
+    ctx.strokeStyle = 'rgba(139, 0, 0, 0.3)';
+    ctx.lineWidth = 2;
+  
+    // Draw diagonal lines
+    for (let i = -canvas.width; i < canvas.width; i += 10) {
+      ctx.beginPath();
+      ctx.moveTo(i, 0);
+      ctx.lineTo(i + canvas.width, canvas.height);
+      ctx.stroke();
     }
-
-    const circles = [];
-    for (let i = 0; i < 100; i++) {
-        const x = Math.random() * window.innerWidth;
-        const y = Math.random() * window.innerHeight;
-        const radius = Math.random() * 30 + 1;
-        circles.push(new Circle(x, y, radius));
-    }
-
-    function animate() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        for (const circle of circles) {
-            circle.draw();
-        }
-
-        requestAnimationFrame(animate);
-    }
-
-    animate();
-})();
+  
+    // Set up chat box opacity
+    const chatBox = document.querySelector('.chat-container');
+    chatBox.style.backgroundColor = 'rgba(255, 255, 255, 0.8)';
+};
